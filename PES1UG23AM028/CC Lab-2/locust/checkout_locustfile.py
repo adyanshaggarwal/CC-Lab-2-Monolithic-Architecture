8c85a6cd2ea126d16726aa8e4b19d51050bb6dcf
AdyanshAggarwal <adyanshaggarwal@gmail.com>
2026-01-20 19:52:04 +0530

    first

---
+++ PES1UG23AM028/CC Lab-2/locust/checkout_locustfile.py
@@ -0,0 +1,9 @@
+from locust import HttpUser, task, between
+
+class CheckoutUser(HttpUser):
+    wait_time = between(1, 2)
+    host = "http://localhost:8089"
+
+    @task
+    def checkout(self):
+        self.client.get("/checkout")
